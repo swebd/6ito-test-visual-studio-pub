0a332d40bae365385f9ef928673ea98e7ab4ed79
swebd <swebd@hotmail.com>
2024-03-27 21:05:15 +0100

    first commit console files

---
+++ Test-console-visual-studio/Test-console-visual-studio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_console_visual_studio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Naam:
+            // Klas:
+
+ 
+
+
+        }
+    }
+}
